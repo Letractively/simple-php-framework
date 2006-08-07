6c4e3d8a3ed4065f4f15cf9ab0c15b89e685ef2e
tylerhall <tylerhall@cc7ac75e-9719-0410-812b-05e975a4e153>
2006-08-07 21:11:20 +0000

    Revamped the database class code.

--- trunk/includes/class.database.php
+++ trunk/includes/class.database.php
@@ -1,27 +1,40 @@
 <?PHP
 	class Database
 	{
+		var $onError   = 0; // 0 = die($error), 1 = email($error) & continue, 2 = do nothing & continue
+		var $longQuery = 0; // 0 = ignore, otherwise email($query) if query time > $longQuery
+		var $errorTo   = "email@domain.com";
+		var $errorFrom = "errors@domain.com";
+		var $errorPage = "http://domain.com/database-error.php";
+
+		var $host;
+		var $user;
+		var $password;
+		var $dbname;
 		var $db;
 		var $result;
 		var $queries;
 
-		var $notifyOnError; // 1 = die($error), 2 = email($error) & continue, 3 = do nothing & continue
-		var $notifyOnLongQuery; // 0 = no, else = email($query) if query time > $notifyOnLongQuery
-
-		var $errorTo = "email@domain.com";
-		var $errorFrom = "errors@domain.com";
-
-		function Database($host, $user, $password, $dbname, $notifyOnError = 1)
+		function Database($host, $user, $password, $dbname)
 		{
-			$this->notifyOnError     = $notifyOnError;
-			$this->notifyOnLongQuery = 0;
-			
-			$this->db = mysql_connect($host, $user, $password) or $this->notify(mysql_error());
-			mysql_select_db($dbname, $this->db) or $this->notify(mysql_error());
+			$this->host     = $host;
+			$this->user     = $user;
+			$this->password = $password;
+			$this->dbname   = $dbname;			
+		}
+		
+		function connect($redirect = false)
+		{
 			$this->queries = array();
+
+			$this->db = mysql_connect($host, $user, $password)
+				or $this->notify(mysql_error(), $redirect);
+
+			mysql_select_db($dbname, $this->db)
+				or $this->notify(mysql_error(), $redirect);
 		}
 
-		function query($sql)
+		function query($sql, $redirect = false)
 		{
 			$this->queries[] = $sql;
 
@@ -31,7 +44,7 @@
 
 			$delta = $stop - $start;
 			
-			if(($notifyOnLongQuery != 0) && ($delta > $notifyOnLongQuery))
+			if(($longQuery != 0) && ($delta > $longQuery))
 			{
 				$msg  = $_SERVER['PHP_SELF'] . " @ " . date("Y-m-d H:ia") . "\n\n";
 				$msg .= "The following query took $delta to complete:\n\n";
@@ -39,30 +52,56 @@
 				$msg .= $this->queries() . "\n\n";
 				mail($this->errorTo, "Long Query " . $_SERVER['PHP_SELF'], $msg, "From: {$this->errorFrom}");
 			}
+			
+			return $db->result;
 		}
 
-		function numQueries() { return count($this->queries); }
-		function lastQuery()  {	return $this->queries[count($this->queries) - 1]; }
-		function queries()    { return implode("\n", $this->queries); }
-		function isValid()    { return isset($this->result) && (mysql_num_rows($this->result) > 0); }
-
-		function notify($err)
+		function numQueries()
 		{
-			if($this->notifyOnError == 1)
-				die($err . "<br/><br/>" . $this->lastQuery());
-			elseif($this->notifyOnError == 2)
+			return count($this->queries);
+		}
+
+		function lastQuery()
+		{
+			return $this->queries[count($this->queries) - 1];
+		}
+
+		function queries()
+		{
+			return implode("\n", $this->queries);
+		}
+		
+		function isValid() 
+		{
+			return isset($this->result) && (mysql_num_rows($this->result) > 0);
+		}
+
+		function notify($errMsg, $redirect)
+		{
+			global $auth;
+
+			switch($this->onError)
 			{
-				global $auth;
-				$msg  = $_SERVER['PHP_SELF'] . " @ " . date("Y-m-d H:ia") . "\n\n";
-				$msg .= $err . "\n\n";
-				$msg .= $this->queries() . "\n\n";
-
-				$msg .= "CURRENT USER\n============\n"     . var_export($auth, true)  . "\n" . $_SERVER['REMOTE_ADDR'] . "\n\n";
-				$msg .= "POST VARIABLES\n==============\n" . var_export($_POST, true) . "\n\n";
-				$msg .= "GET VARIABLES\n=============\n"   . var_export($_GET, true)  . "\n\n";
-
-				mail($this->errorTo, $_SERVER['PHP_SELF'], $msg, "From: {$this->errorFrom}");
+				case 0:
+					die($err . "<br/><br/>" . $this->lastQuery());
+					break;
+				
+				case 1:
+					$msg  = $_SERVER['PHP_SELF'] . " @ " . date("Y-m-d H:ia") . "\n";
+					$msg .= $errMsg . "\n\n";
+					$msg .= $this->queries() . "\n\n";
+					$msg .= "CURRENT USER\n============\n"     . var_export($auth, true)  . "\n" . $_SERVER['REMOTE_ADDR'] . "\n\n";
+					$msg .= "POST VARIABLES\n==============\n" . var_export($_POST, true) . "\n\n";
+					$msg .= "GET VARIABLES\n=============\n"   . var_export($_GET, true)  . "\n\n";
+					mail($this->errorTo, $_SERVER['PHP_SELF'], $msg, "From: {$this->errorFrom}");
+					break;
 			}
+			
+			if($redirect)
+			{
+				header("Location: {$this->errorPage}");
+				exit();
+			}			
 		}
 	}
 ?>
