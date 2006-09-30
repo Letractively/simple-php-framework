21c02bf0bc499ec3caee7f5fa4fb5ed263d00df3
tylerhall <tylerhall@cc7ac75e-9719-0410-812b-05e975a4e153>
2006-09-30 20:35:01 +0000

    Added hourmin() to functions.inc.php

--- trunk/includes/functions.inc.php
+++ trunk/includes/functions.inc.php
@@ -12,6 +12,35 @@
 			return $phone;
 	}
 
+	// Outputs hour, minute, am/pm dropdown boxes
+	function hourmin($hid = "hour", $mid = "minute", $pid = "pm", $hval = "", $mval = "", $pval = "")
+	{
+		if(empty($hval)) $hval = date("h");
+		if(empty($mval)) $mval = date("i");
+		if(empty($pval)) $pval = date("a");
+
+		$hours = array(12, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11);
+		$out = "<select name='$hid' id='$hid'>";
+		foreach($hours as $hour)
+			if(intval($hval) == intval($hour)) $out .= "<option value='$hour' selected>$hour</option>";
+			else $out .= "<option value='$hour'>$hour</option>";
+		$out .= "</select>";
+
+		$minutes = array("00", 15, 30, 45);
+		$out .= "<select name='$mid' id='$mid'>";
+		foreach($minutes as $minute)
+			if(intval($mval) == intval($minute)) $out .= "<option value='$minute' selected>$minute</option>";
+			else $out .= "<option value='$minute'>$minute</option>";
+		$out .= "</select>";
+		
+		$out .= "<select name='$pid' id='$pid'>";
+		$out .= "<option value='am'>am</option>";
+		if($pval == "pm") $out .= "<option value='pm' selected>pm</option>";
+		else $out .= "<option value='pm'>pm</option>";
+		
+		return $out;
+	}
+
 	// Outputs month, day, and year dropdown boxes with default values and custom id/names
 	function mdy($mid = "month", $did = "day", $yid = "year", $mval = "", $dval = "", $yval = "")
 	{
