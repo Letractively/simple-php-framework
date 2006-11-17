fda84696efc576fb933e76a3907e2ee3127d07be
tylerhall <tylerhall@cc7ac75e-9719-0410-812b-05e975a4e153>
2006-11-17 17:32:51 +0000

    Collin's changes broke $user loading. Reverting back to previous version.

--- trunk/includes/class.dbobject.php
+++ trunk/includes/class.dbobject.php
@@ -1,27 +1,59 @@
 <?php
+
+	//
+	// class User extends DBObject
+	// {
+	//	function __construct($id = "")
+	// 	{
+	// 		parent::__construct('users', 'user_id', array('username', 'password', 'level', 'email'), $id);
+	// 	}
+	// }
+	//
+
 	class DBObject
 	{
 		public $id;
-		private $__id_name;
-		private $__table_name;
-		private $__columns = array();
+		private $id_name;
+		private $table_name;
+		private $columns = array();
 
 		function __construct($table_name, $id_name, $columns, $id = "")
 		{
-			$this->__table_name = $table_name;
-			$this->__id_name = $id_name;
+			$this->table_name = $table_name;
+			$this->id_name = $id_name;
 
-			$this->__columns = $columns;
+			foreach($columns as $key)
+				$this->columns[$key] = null;
 				
-			if(!empty($id))
+			if($id != "")
 				$this->select($id);
 		}
 
-		function select($id)
+		function __get($key)
+		{
+			return $this->columns[$key];
+		}
+
+		function __set($key, $value)
+		{
+			if(array_key_exists($key, $this->columns))
+			{
+				$this->columns[$key] = $value;
+				return true;
+			}
+			return false;
+		}
+
+		function select($id, $column = "")
 		{
 			global $db;
+			
+			if($column == "") $column = $this->id_name;
 
-			$db->query("SELECT ".join(',', $this->__columns)." FROM {$this->__table_name} WHERE {$this->__id_name}='$id'");
+			$id = mysql_real_escape_string($id, $db->db);
+			$column = mysql_real_escape_string($column, $db->db);
+
+			$db->query("SELECT * FROM " . $this->table_name . " WHERE `$column` = '$id'");
 			if(mysql_num_rows($db->result) == 0)
 				return false;
 			else
@@ -29,26 +61,26 @@
 				$this->id = $id;
 				$row = mysql_fetch_array($db->result, MYSQL_ASSOC);
 				foreach($row as $key => $val)
-					$this->$key = $val;
+					$this->columns[$key] = $val;
 			}
 		}
 
 		function insert()
 		{
 			global $db;
-
-			unset($this->__columns[$this->__id_name]);
-			$columns = join(", ", $this->__columns);
 			
-			$values = array();
-			foreach($this->__columns as $col)
-				$values[] = $db->quote($this->$col);
-			$values = join(',', $values);
+			if(count($this->columns) > 0)
+			{
+				unset($this->columns[$this->id_name]);
 
-			$db->query("INSERT INTO " . $this->__table_name . " ($columns) VALUES ($values)");
+				$columns = "`" . join("`, `", array_keys($this->columns)) . "`";
+				$values  = "'" . join("', '", $this->quote_column_vals()) . "'";
 
-			$this->id = mysql_insert_id($db->db);
-			return $this->id;
+				$db->query("INSERT INTO " . $this->table_name . " ($columns) VALUES ($values)");
+
+				$this->id = mysql_insert_id($db->db);
+				return $this->id;
+			}
 		}
 
 		function update()
@@ -56,43 +88,34 @@
 			global $db;
 
 			$arrStuff = array();
-			unset($this->__columns[$this->__id_name]);
-			foreach($this->__columns as $key)
-				$arrStuff[] = $key.'='.$db->quote($this->$key);
-			$stuff = join(',', $arrStuff);
+			unset($this->columns[$this->id_name]);
+			foreach($this->quote_column_vals() as $key => $val)
+				$arrStuff[] = "`$key` = '$val'";
+			$stuff = implode(", ", $arrStuff);
+			
+			$id = mysql_real_escape_string($this->id, $db->db);
 		
-			$db->query("UPDATE " . $this->__table_name . " SET $stuff WHERE " . $this->__id_name . " = '" . $this->id . "'");
+			$db->query("UPDATE " . $this->table_name . " SET $stuff WHERE " . $this->id_name . " = '" . $id . "'");
 			return mysql_affected_rows($db->db); // Not always correct due to mysql update bug/feature
 		}
 
 		function delete()
 		{
 			global $db;
-			$db->query("DELETE FROM " . $this->__table_name . " WHERE " . $this->__id_name . " = '" . $this->id . "'");
+			$id = mysql_real_escape_string($this->id, $db->db);
+			$db->query("DELETE FROM " . $this->table_name . " WHERE `" . $this->id_name . "` = '" . $id . "'");
 			return mysql_affected_rows($db->db);
 		}
 		
-		function postload()
-		{
-			foreach($_POST as $key => $val)
-				$_POST[$key] = fix_slashes($val);
-			$this->load($_POST);
-		}
-		
-		function getload()
-		{
-			foreach($_GET as $key => $val)
-				$_GET[$key] = fix_slashes($val);
-			$this->load($_GET);
-		}
-
+		function postload() { $this->load($_POST); }
+		function getload()  { $this->load($_GET); }
 		function load($arr)
 		{
 			if(is_array($arr))
 			{
 				foreach($arr as $key => $val)
-					if(array_key_exists($key, $this->__columns))
-						$this->__columns[$key] = $val;
+					if(array_key_exists($key, $this->columns) && $key != $this->id_name)
+						$this->columns[$key] = fix_slashes($val);
 				return true;
 			}
 			else
@@ -103,8 +126,8 @@
 		{
 			global $db;
 			$columnVals = array();
-			foreach($this->__columns as $key)
-				$columnVals[$key] = mysql_real_escape_string($this->$key, $db->db);
+			foreach($this->columns  as $key => $val)
+				$columnVals[$key] = mysql_real_escape_string($val, $db->db);
 			return $columnVals;
 		}
 	}
