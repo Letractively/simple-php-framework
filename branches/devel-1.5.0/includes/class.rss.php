f0ef1a6928cf11ad164007457c1cee133d0c231f
tylerhall <tylerhall@cc7ac75e-9719-0410-812b-05e975a4e153>
2008-10-04 05:00:09 +0000

    Cleaned RSS class

--- branches/devel-1.5.0/includes/class.rss.php
+++ branches/devel-1.5.0/includes/class.rss.php
@@ -1,201 +1,194 @@
 <?PHP
+	class RSS
+	{
+	    public $title;
+	    public $link;
+	    public $description;
+	    public $language = 'en-us';
+	    public $pubDate;
+		public $url;
+	    public $items;
+	    public $tags;
+		public $useCDataTags;
 
-    /* E X A M P L E -----------------------------------------------
-        $feed = new RSS();
-        $feed->title       = 'RSS Feed Title';
-        $feed->link        = "http://website.com";
-        $feed->description = 'Recent articles on your website.';
+	    public function __construct()
+	    {
+	        $this->items = array();
+	        $this->tags  = array();
+			$this->useCDataTags = true;
+			$this->setPubDate();
+			$this->url = $this->fullUrl();
+	    }
 
-        $db->query($query);
-        $result = $db->result;
-        while($row = mysql_fetch_array($result, MYSQL_ASSOC))
-        {
-            $item = new RSSItem();
-            $item->title = $title;
-            $item->link  = $link;
-            $item->setPubDate($create_date);
-            $item->description = $html;
-            $feed->addItem($item);
-        }
-        $feed->serve();
+	    public function addItem($item)
+	    {
+	        $this->items[] = $item;
+	    }
 
-        Or, you can eliminate the while loop above by using the
-        built-in loadRecordset() method. It takes a MySQL result
-        and the column names you want to use for the title, link,
-        description, and pub-date.
-    ---------------------------------------------------------------- */
+	    public function setPubDate($date = null)
+	    {
+			if(is_null($date)) $date = time();
+			if(!ctype_digit($date)) $date = strtotime($date);
+			$this->pubDate = date('D, d M Y H:i:s O', $date);
+	    }
 
-    class RSS
-    {
-        public $title;
-        public $link;
-        public $description;
-        public $language = 'en-us';
-        public $pubDate;
-        public $items;
-        public $tags;
+	    public function addTag($tag, $value)
+	    {
+	        $this->tags[$tag] = $value;
+	    }
 
-        public function __construct()
-        {
-            $this->items = array();
-            $this->tags  = array();
-        }
+	    public function loadRecordset($result, $title, $link, $description, $pub_date)
+	    {
+	        while($row = mysql_fetch_array($result, MYSQL_ASSOC))
+	        {
+	            $item = new RSSItem();
+	            $item->title       = $row[$title];
+	            $item->link        = $row[$link];
+	            $item->description = $row[$description];
+	            $item->setPubDate($row[$pub_date]);
+	            $this->addItem($item);
+	        }
+	    }
 
-        public function addItem($item)
-        {
-            $this->items[] = $item;
-        }
+	    public function out()
+	    {
+	        $bad         = array('&', '<');
+	        $good        = array('&#x26;', '&#x3c;');
+	        $title       = str_replace($bad, $good, $this->title);
+	        $description = str_replace($bad, $good, $this->description);    
 
-        public function setPubDate($when)
-        {
-            if(empty($when) || ctype_digit($date) === true)
-                $this->pubDate = date("D, d M Y H:i:s O", $when);
-            else
-                $this->pubDate = date("D, d M Y H:i:s O", strtotime($when));
-        }
+	        $out  = $this->header();
+	        $out .= "<channel>\n";
+	        $out .= "<title>" . $title . "</title>\n";
+	        $out .= "<link>" . $this->link . "</link>\n";
+	        $out .= "<description>" . $description . "</description>\n";
+	        $out .= "<language>" . $this->language . "</language>\n";
+	        $out .= "<pubDate>" . $this->pubDate . "</pubDate>\n";
+	        $out .= '<atom:link href="' . $this->url . '" rel="self" type="application/rss+xml" />' . "\n";
 
-        public function getPubDate()
-        {
-            if(empty($this->pubDate))
-                return date("D, d M Y H:i:s O");
-            else
-                return $this->pubDate;
-        }
+	        foreach($this->tags as $k => $v)
+				$out .= "<$k>$v</$k>\n";
 
-        public function addTag($tag, $value)
-        {
-            $this->tags[$tag] = $value;
-        }
+	        foreach($this->items as $item)
+				$out .= $item->out();
 
-        public function loadRecordset($result, $title, $link, $description, $pub_date)
-        {
-            while($row = mysql_fetch_array($result, MYSQL_ASSOC))
-            {
-                $item = new RSSItem();
-                $item->title       = $row[$title];
-                $item->link        = $row[$link];
-                $item->description = "<![CDATA[ " . $row[$description] . "]]>";
-                $item->setPubDate($row[$pub_date]);
-                $this->addItem($item);
-            }
-        }
+	        $out .= "</channel>\n";
 
-        public function out()
-        {
-            $out  = $this->header();
-            $out .= "<channel>\n";
-            $out .= "<title>" . $this->title . "</title>\n";
-            $out .= "<link>" . $this->link . "</link>\n";
-            $out .= "<description>" . $this->description . "</description>\n";
-            $out .= "<language>" . $this->language . "</language>\n";
-            $out .= "<pubDate>" . $this->getPubDate() . "</pubDate>\n";
-            $out .= '<atom:link href="' . $this->full_url() . '" rel="self" type="application/rss+xml" />' . "\n";
+	        $out .= $this->footer();
 
-            foreach($this->tags as $key => $val) $out .= "<$key>$val</$key>\n";
-            foreach($this->items as $item) $out .= $item->out();
+	        return $out;
+	    }
 
-            $out .= "</channel>\n";
+	    public function serve($contentType = 'application/xml')
+	    {
+	        $xml = $this->out();
+	        header("Content-type: $contentType");
+	        echo $xml;
+	    }
 
-            $out .= $this->footer();
+	    private function header()
+	    {
+	        $out  = '<?xml version="1.0" encoding="utf-8"?>' . "\n";
+	        $out .= '<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">' . "\n";
+	        return $out;
+	    }
 
-            return $out;
-        }
+	    private function footer()
+	    {
+	        return '</rss>';
+	    }
 
-        public function serve($contentType = 'application/xml')
-        {
-            $xml = $this->out();
-            header("Content-type: $contentType");
-            echo $xml;
-        }
+	    private function fullUrl()
+	    {
+	        $s = empty($_SERVER['HTTPS']) ? '' : ($_SERVER['HTTPS'] == 'on') ? 's' : '';
+	        $protocol = substr(strtolower($_SERVER['SERVER_PROTOCOL']), 0, strpos(strtolower($_SERVER['SERVER_PROTOCOL']), '/')) . $s;
+	        $port = ($_SERVER['SERVER_PORT'] == '80') ? '' : (":".$_SERVER['SERVER_PORT']);
+	        return $protocol . "://" . $_SERVER['HTTP_HOST'] . $port . $_SERVER['REQUEST_URI'];
+	    }
 
-        public function header()
-        {
-            $out  = '<?xml version="1.0" encoding="utf-8"?>' . "\n";
-            $out .= '<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">' . "\n";
-            return $out;
-        }
+		private function cdata($str)
+		{
+			if($this->useCDataTags)
+			{
+				$str = '<![CDATA[' . $str . ']]>';
+			}
+			return $str;
+		}
+	}
 
-        public function footer()
-        {
-            return '</rss>';
-        }
+	class RSSItem
+	{
+	    public $title;
+	    public $link;
+	    public $description;
+	    public $pubDate;
+	    public $guid;
+	    public $tags;
+	    public $enclosureUrl;
+	    public $enclosureType;
+	    public $enclosureLength;
+		public $useCDataTags;
 
-        public function full_url()
-        {
-            $s = empty($_SERVER['HTTPS']) ? '' : ($_SERVER['HTTPS'] == 'on') ? 's' : '';
-            $protocol = substr(strtolower($_SERVER['SERVER_PROTOCOL']), 0, strpos(strtolower($_SERVER['SERVER_PROTOCOL']), '/')) . $s;
-            $port = ($_SERVER['SERVER_PORT'] == '80') ? '' : (":".$_SERVER['SERVER_PORT']);
-            return $protocol . "://" . $_SERVER['HTTP_HOST'] . $port . $_SERVER['REQUEST_URI'];
-        }
-    }
+	    public function __construct()
+	    {
+			$this->useCDataTags = true;
+	        $this->tags = array();
+			$this->setPubDate();
+	    }
 
-    class RSSItem
-    {
-        public $title;
-        public $link;
-        public $description;
-        public $pubDate;
-        public $guid;
-        public $tags;
-        public $attachment;
-        public $length;
-        public $type;
+	    public function setPubDate($date = null)
+	    {
+			if(is_null($date)) $date = time();
+			if(!ctype_digit($date)) $date = strtotime($date);
+			$this->pubDate = date('D, d M Y H:i:s O', $date);
+	    }
 
-        public function __construct()
-        {
-            $this->tags = array();
-        }
+	    public function addTag($tag, $value)
+	    {
+	        $this->tags[$tag] = $value;
+	    }
 
-        public function setPubDate($when)
-        {
-            if(empty($when) || strtotime($when) == false)
-                $this->pubDate = date("D, d M Y H:i:s O", $when);
-            else
-                $this->pubDate = date("D, d M Y H:i:s O", strtotime($when));
-        }
+	    public function out()
+	    {
+	        $bad         = array('&', '<');
+	        $good        = array('&#x26;', '&#x3c;');
+	        $title       = str_replace($bad, $good, $this->title);
+	        $description = str_replace($bad, $good, $this->description);
 
-        public function getPubDate()
-        {
-            if(empty($this->pubDate))
-                return date("D, d M Y H:i:s O");
-            else
-                return $this->pubDate;
-        }
+	        $out  = "<item>\n";
+	        $out .= "<title>" . $title . "</title>\n";
+	        $out .= "<link>" . $this->link . "</link>\n";
+	        $out .= "<description>" . $this->cdata($description) . "</description>\n";
+	        $out .= "<pubDate>" . $this->pubDate . "</pubDate>\n";
 
-        public function addTag($tag, $value)
-        {
-            $this->tags[$tag] = $value;
-        }
+	        if(is_null($this->guid))
+				$this->guid = $this->link;
 
-        public function out()
-        {
-            $bad = array('&', '<');
-            $good = array('&#x26;', '&#x3c;');
+	        $out .= "<guid>" . $this->guid . "</guid>\n";
 
-            $title = str_replace($bad, $good, $this->title);
-            $description = str_replace($bad, $good, $this->description);
+	        if(!is_null($this->enclosureUrl))
+	            $out .= "<enclosure url='{$this->enclosureUrl}' length='{$this->enclosureLength}' type='{$this->enclosureType}' />\n";
 
-            $out  = "<item>\n";
-            $out .= "<title>" . $title . "</title>\n";
-            $out .= "<link>" . $this->link . "</link>\n";
-            $out .= "<description>" . $description . "</description>\n";
-            $out .= "<pubDate>" . $this->getPubDate() . "</pubDate>\n";
+	        foreach($this->tags as $k => $v)
+				$out .= "<$k>$v</$k>\n";
 
-            if(empty($this->guid)) $this->guid = $this->link;
-            $out .= "<guid>" . $this->guid . "</guid>\n";
+	        $out .= "</item>\n";
+	        return $out;
+	    }
 
-            if($this->attachment != '')
-                $out .= "<enclosure url='{$this->attachment}' length='{$this->length}' type='{$this->type}' />\n";
+	    public function enclosure($url, $type, $length)
+	    {
+	        $this->enclosureUrl    = $url;
+	        $this->enclosureType   = $type;
+	        $this->enclosureLength = $length;
+	    }
 
-            foreach($this->tags as $key => $val) $out .= "<$key>$val</$key>\n";
-            $out .= "</item>\n";
-            return $out;
-        }
-
-        public function enclosure($url, $type, $length)
-        {
-            $this->attachment = $url;
-            $this->type       = $type;
-            $this->length     = $length;
-        }
-    }
+		private function cdata($str)
+		{
+			if($this->useCDataTags)
+			{
+				$str = '<![CDATA[' . $str . ']]>';
+			}
+			return $str;
+		}
+	}
